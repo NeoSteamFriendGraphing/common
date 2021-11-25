c28afadb30ab7afec9c159776b0806517160bf3d
IamCathal <cathalsocallaghan@gmail.com>
2021-11-25 20:51:25 +0000

    Responses: Add BasicResponse

--- apiResponses.go
+++ apiResponses.go
@@ -8,3 +8,11 @@ type UptimeResponse struct {
 	Status string        `json:"status"`
 	Uptime time.Duration `json:"uptime"`
 }
+
+// BasicAPIResponse is the basic API response
+// for endpoints that just need a response
+// rather than data to be returned
+type BasicAPIResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
